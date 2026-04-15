ee49b2eef70b6f7744b630edd2c66334e45f0d10
Bunnies228 <jingchia228@gmail.com>
2026-04-15 23:24:22 +0800

    Increase Json limit

--- src/index.ts
+++ src/index.ts
@@ -236,8 +236,8 @@ app.use(cors({ origin: '*' }));
 app.get('/', (req, res) => {
     res.send('🚀 CTRL+V AI Farming Backend is LIVE and running smoothly!');
 });
-app.use(cors()); // to connect with frontend
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 //Agent 1  
 app.post('/api/disease-detect', async (req, res) => {
