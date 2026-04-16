768ae9dd210810f5dc1ab4c91146a5376cc0369c
Bunnies228 <jingchia228@gmail.com>
2026-04-16 10:54:06 +0800

    add limits

--- src/index.ts
+++ src/index.ts
@@ -245,6 +245,7 @@ app.use(cors({ origin: '*' }));
 app.get('/', (req, res) => {
     res.send('🚀 CTRL+V AI Farming Backend is LIVE and running smoothly!');
 });
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
